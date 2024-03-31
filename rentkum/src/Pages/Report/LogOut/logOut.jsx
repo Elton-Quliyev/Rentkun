6bf7fb263594eb1c3a5edd470c64aa0c68f2a7a2
Elton-Quliyev <equliyev118@gmail.com>
2024-04-01 02:16:10 +0400

    .

--- rentkum/src/Pages/Report/LogOut/logOut.jsx
+++ rentkum/src/Pages/Report/LogOut/logOut.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './logOut.scss'
 
 export default function LogOut() {
   return (
--- rentkum/src/Shared/Header/header.jsx
+++ rentkum/src/Shared/Header/header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import './header.scss'
 import { Link, Outlet } from 'react-router-dom';
 import Logo from '../../assets/headerPhoto/Logo.png'
+import Dashboard from '../../assets/headerIcons/dashboard.svg'
+import Discover from '../../assets/headerIcons/discover.svg'
+import Calendar from '../../assets/headerIcons/calendar.svg'
+import Save from '../../assets/headerIcons/save.svg'
+import inbox from '../../assets/headerIcons/inbox.svg'
+import Transactions from '../../assets/headerIcons/transactions.svg'
+import CarReports from '../../assets/headerIcons/carReports.svg'
+import Setting from '../../assets/headerIcons/setting.svg'
+import Logout from '../../assets/headerIcons/logout.svg'
+import ShoppingIcon from '../../assets/headerIcons/shopping-cart.svg'
+
 
 export default function Header() {
   return (
-    <header>
+    <header className='header'>
        <div className="tittle">
             <img 
                 src={Logo} 
@@ -15,29 +27,62 @@ export default function Header() {
 
        <div className='sidebar'>
 
-            <h2 className='aboutCar'>ABOUT CAR</h2>
+            <h2 className='half-bass'>ABOUT CAR</h2>
 
             <ul className='sidebar-list'>
-                <Link className='sidebar-list_item'  to='/'>Dashboard</Link>
-                <Link className='sidebar-list_item'  to='/discover'>Discover</Link>
-                <Link className='sidebar-list_item'  to='/calendar'>Calendar</Link>
-                <Link className='sidebar-list_item'  to='/saved'>Saved</Link>
-                <Link className='sidebar-list_item'  to='/inbox'>Inbox</Link>
+                <Link className='sidebar-list_item'  to='/'>
+                  <img style={{opacity: '0.4'}} className='sidebar-list_item-icon' src={Dashboard} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Dashboard</span>
+                </Link>
+                <Link className='sidebar-list_item'  to='/discover'>
+                  <img className='sidebar-list_item-icon' src={Discover} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Discover</span>
+                </Link>
+                <Link className='sidebar-list_item'  to='/calendar'>
+                  <img className='sidebar-list_item-icon' src={Calendar} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Calendar</span>
+                </Link>
+                <Link className='sidebar-list_item'  to='/saved'>
+                  <img className='sidebar-list_item-icon' src={Save} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Saved</span>
+                </Link>
+                <Link className='sidebar-list_item'  to='/inbox'>
+                  <img className='sidebar-list_item-icon' src={inbox} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Inbox</span>
+                </Link>
 
             </ul>
 
-            <hr />
-            <h2 className='report'>REPORT</h2>
+            <hr className='line' />
+            <h2 className='half-bass'>REPORT</h2>
 
             <ul className='sidebar-list'>
-                <Link className='sidebar-list_item'  to='/transactions'>Transactions</Link>
-                <Link className='sidebar-list_item'  to='/carReports'>Car Reports</Link>
+                <Link className='sidebar-list_item'  to='/transactions'>
+                  <img className='sidebar-list_item-icon' src={Transactions} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Transactions</span>
+                </Link>
+                <Link className='sidebar-list_item'  to='/carReports'>
+                  <img className='sidebar-list_item-icon' src={CarReports} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Car Reports</span>
+                </Link>
             </ul>
 
+            <div className='shopping'>
+              <div className='shopping-card'>
+                <img className='shopping-card_icon' src={ShoppingIcon} alt="shopping-icon" />
+              </div>
+              <span className='shopping-text'>We connect you with thousands of potential buyers in your area.</span>
+              <button className='shopping-btn'>Sell Your Car</button>
+            </div>
 
             <ul className='sidebar-list'>
-                <Link className='sidebar-list_item'  to='/settings'>Settings</Link>
-                <Link className='sidebar-list_item'  to='/logOut'>Log Out</Link>
+                <Link className='sidebar-list_item'  to='/settings'>
+                  <img className='sidebar-list_item-icon' src={Setting} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Settings</span>
+                </Link>
+                <Link style={{color: 'red'}} className='sidebar-list_item '  to='/logOut'>
+                  <img className='sidebar-list_item-icon' src={Logout} alt="dashboard" />
+                  <span className='sidebar-list_item-name'>Log Out</span></Link>
             </ul>
        </div>
     </header>
